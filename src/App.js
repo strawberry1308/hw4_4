4f1efbd2fe59e732abb0b38746501ec4f370a17e
strawberry <alinches1308@gmail.com>
2023-06-05 15:05:24 +0700

    hw4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function RegistrationForm() {
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`ФИО: ${fullName}\nТелефон: ${phone}\nEmail: ${email}`);
+    alert(`ФИО: ${fullName}\nТелефон: ${phone}\nEmail: ${email}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit} className="registration-form">
+        <h2>Регистрация</h2>
+        <label>
+          ФИО:
+          <input
+              type="text"
+              value={fullName}
+              onChange={(event) => setFullName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Телефон:
+          <input
+              type="tel"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Зарегистрироваться</button>
+      </form>
   );
 }
 
-export default App;
+export default RegistrationForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+//
+// export default class NameForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ""}
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit= this.handleSubmit.bind(this)
+//   }
+//   handleChange(event) {
+//     this.setState({value: event.target.value})
+//   }
+//   handleSubmit(event) {
+//     alert("Default message:" + this.state.value)
+//     event.preventDefault()
+//   }
+//   render() {
+//     return(
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Name:
+//             <input type="text" value={this.state.value} onChange={this.handleChange}/>
+//           </label>
+//           <input type="submit" value="Send message"/>
+//         </form>
+//     )
+//   }
+// }
+//
+//
+// // export default NameForm;
